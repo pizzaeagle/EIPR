8c4b50339e9d5ce02303bc1529a0367cc54f71d3
michalmst <michalmst@gmail.com>
2021-02-26 15:26:41 +0100

    Small fix

--- local_run.sh
+++ local_run.sh
@@ -10,4 +10,4 @@ spark-submit --properties-file src/main/resources/config.properties \
   --class mm.ip.Main \
   target/scala-2.11/EIPR-assembly-0.1.jar
 
-open http://localhost:5601/app/kibana#/home?_g=()
+open http://localhost:5601/app/kibana#/home
